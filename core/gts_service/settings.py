0d22a6fb08d47230cdf72a8d9270df538c6e8612
ryancho_ <73058499+JustDevRyan@users.noreply.github.com>
2026-03-01 18:06:05 +0200

    Update settings.py

--- core/gts_service/settings.py
+++ core/gts_service/settings.py
@@ -68,8 +68,8 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 LANGUAGE_CODE = "en-us"
 TIME_ZONE = "UTC"
-USE_I18N = True
-USE_L10N = True
+USE_I18N = False  # Disable Internationalization
+USE_L10N = False  # Disable Localization
 USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
