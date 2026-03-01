80d1d5d918456eec27baa473a91c130012e71994
ryancho_ <73058499+JustDevRyan@users.noreply.github.com>
2026-03-01 16:41:51 +0200

    Delete gerber_to_scad directory

--- gerber_to_scad/__init__.py
+++
@@ -1,5 +0,0 @@
-from .conversion import process_gerber
-from .cli import gerber_to_scad_cli
-
-if __name__ == "__main__":
-    gerber_to_scad_cli()
--- gerber_to_scad/__main__.py
+++
@@ -1,6 +0,0 @@
-"""`__main__.py` file adds support for running like: `python -m gerber_to_scad -h`."""
-
-from .cli import gerber_to_scad_cli
-
-if __name__ == "__main__":
-    gerber_to_scad_cli()
